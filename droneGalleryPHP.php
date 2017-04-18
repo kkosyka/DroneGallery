a63d64a445864a39e0c1cce02112701588d074b7
Kalynn <KKOSYKA@Kalynns-MacBook-Pro.local>
2017-04-17 21:55:12 -0400

    working on adding data.

---
+++ droneGalleryPHP.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>Your (Ref No: ["addLocation"]) has been submitted.</title>        
+  </head>
+	<?php
+	    if(isset($_GET['mainid'])){
+	        mainInfo($_GET['mainid']);
+	    }
+	?>
+</html>
