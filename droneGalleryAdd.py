311bb132264cae062c698032e51d4342837dfb99
Kalynn <KKOSYKA@Kalynns-MacBook-Pro.local>
2017-09-27 21:36:11 -0400

    Fix User Side

--- droneGalleryAdd.py
+++ droneGalleryAdd.py
@@ -3,7 +3,7 @@
 import os, sys, json
 from Tkinter import *
 
-
+file = "droneGalleryJSON1.json"
 
 root = Tk()
 addLabel = Label( root, text="ADD", font=(None, 25))
@@ -19,101 +19,118 @@ xCoorEntry = Entry(root, bd =5)
 yCoorText = Label( root, text="Y-Coor")
 yCoorEntry = Entry(root, bd =5)
 
-def clear_textbox(): #see if it can clear more than one entry...
+def clear(): #see if it can clear more than one entry...
 	layerNameEntry.delete(0, END)
-    # descriptionEntry.delete("1.0", END)
-    # dateEntry.delete(0, END)
-    # xCoorEntry.delete(0, END)
-    # yCoorEntry.delete(0, END)
+	clear1()
+	clear2()
+	clear3()
+	clear4()
+def clear1():
+    descriptionEntry.delete("1.0", END)
+def clear2():
+	dateEntry.delete(0, END)
+def clear3():
+    xCoorEntry.delete(0, END)
+def clear4():
+    yCoorEntry.delete(0, END)
 
 def deleteLastLine():
-	readFile = open("droneGalleryJSON.json")
+	readFile = open(file)
 	lines = readFile.readlines()
 	readFile.close()
-
-	w = open("droneGalleryJSON.json",'w')
-
+	w = open(file,'w')
 	w.writelines([item for item in lines[:-2]])
 	w.writelines("]")
 	w.close()
 
+def display(description, date, xcoor,ycoor):
+	descriptionEntry.insert("1.0", description)
+	dateEntry.insert(0, date)
+	xCoorEntry.insert(0, xcoor)
+	yCoorEntry.insert(0, ycoor)
+
+
 def displayLine(changeLayer): #edit line - when JSON dumps, it changes the order...
 	data = []
-	if os.path.exists('droneGalleryJSON.json'):
-	    with open('droneGalleryJSON.json', 'r+') as f:
+	if os.path.exists(file):
+	    with open(file, 'r+') as f:
 	    	playlist = json.load(f)
 	    	for i in range(len(playlist)):
 	        	if (playlist[i]["layer"] == changeLayer):
-	        		layerNameEntry.insert(0, playlist[i]["layer"])
-					# descriptionEntry.insert("1.0", playlist[i]["description"])
-					# dateEntry.insert(0, playlist[i]["date"])
-					# xCoorEntry.insert(0, playlist[i]["xcoor"])
-					# yCoorEntry.insert(0, playlist[i]["ycoor"])
+	        		layerNameEntry.insert(1, playlist[i]["layer"])
+	        		display(playlist[i]["description"], playlist[i]["date"], playlist[i]["xcoor"], playlist[i]["ycoor"])
+		# 			# descriptionEntry.insert("1.0", playlist[i]["description"])
+		# 			# dateEntry.insert(0, playlist[i]["date"])
+		# 			# xCoorEntry.insert(0, playlist[i]["xcoor"])
+		# 			# yCoorEntry.insert(0, playlist[i]["ycoor"])
+		# 			# playlist[i]["layer"] = "PLEASE WORK"
+	 #        		playlist[i]["description"] = "PLEASE WORK"
+	 #        		playlist[i]["date"] = "PLEASE WORK"
+	 #        		playlist[i]["xcoor"] = "PLEASE WORK"
+	 #        		playlist[i]["ycoor"] = "PLEASE WORK"
+	 #        	data.append(playlist[i])
 
-	        		playlist[i]["description"] = "PLEASE WORK"
-	        	data.append(playlist[i])
+		# f.seek(0)
+		# f.writelines("[\n")
+		# f.writelines(["%s\n" % json.dumps(i) for i in data[0:1]])
+		# f.writelines([",%s\n" % json.dumps(i) for i in data[1:]])
+		# f.writelines(",\n]")
+		# f.truncate()
 
-		f.seek(0)
-		f.writelines("[\n")
-		f.writelines(["%s\n" % json.dumps(i) for i in data[0:1]])
-		f.writelines([",%s\n" % json.dumps(i) for i in data[1:]])
-		f.writelines(",\n]")
-		f.truncate()
+def modifyLine():
+	data = []
+	newLayerName = layerNameEntry.get()
+	newDesc = descriptionEntry.get("1.0",'end-1c')
+	newDate = dateEntry.get()
+	newXCoor = xCoorEntry.get()
+	newYCoor = yCoorEntry.get()
+	print ("Modified to: \n" +  newLayerName + " \n" + newDesc + " \n" + newDate + " \n" + newXCoor + "\n" + newYCoor)
+	if os.path.exists(file):
+	    with open(file, 'r+') as f:
+	    	playlist = json.load(f)
+	    	for i in range(len(playlist)):
+	    		layerName = layerNameEntry.get()
+	        	if (playlist[i]["layer"] == layerName):
+					playlist[i]["description"] = newDesc
+					playlist[i]["date"] = newDate
+					playlist[i]["xcoor"] = newXCoor
+					playlist[i]["ycoor"] = newYCoor
+					data.append(playlist)
 
-	# data = []
-	# with open('droneGalleryJSON.json') as f:
-	# 	for line in f:
-	# 		print line
-	# 		data.append(line)
-
-	# data = []
-	# with open('droneGalleryJSON.json', 'r+') as f:
-	#     for line in f:
-	#         data_line = json.loads(line)
-	#         print line
-	#         # if data_line[0] == 'test':
-	#         #     data_line[1] = 'new value'
-	#         # data.append(data_line)
-	#     f.seek(0)
-	#     f.writelines(["%s\n" % json.dumps(i) for i in data])
-	    # f.truncate()
-
-
-
-
+			f.seek(0)
+			out = [json.dumps(i, indent=0, sort_keys = True, separators=(',', ':')) for i in data]
+			f.writelines(out)
+			f.truncate()
 
 def addLine():
-#layerNameEntry.get(), descriptionEntry.get(), dateEntry.get(), xCoorEntry.get(), yCoorEntry.get()
-	# numAdds = int(raw_input("Number of entried to add? "))
-	# for i in range(numAdds):
 	layerName = layerNameEntry.get()
 	description = descriptionEntry.get("1.0",'end-1c')
 	date = dateEntry.get()
 	xcoor = xCoorEntry.get()
 	ycoor = yCoorEntry.get()
 
-	readFile = open("droneGalleryJSON.json")
+	readFile = open(file)
 	lines = readFile.readlines()
 	readFile.close()
-
-	w = open("droneGalleryJSON.json",'w')
-
+	w = open(file,'w')
+	# lastLine = (item for item in lines[-8:-1])
+	#w.writelines([item for item in lines[:-8]])
 	w.writelines([item for item in lines[:-1]])
-	w.writelines(',{"layer": "' + layerName +'", "description": "'+description+'", "date": "' +date+ '", "xcoor": "'+ xcoor+'", "ycoor": "' + ycoor + '"}')
-	print ('ADDED: \nLayer Name:' + layerName +'\nDescription": '+description+'\nDate: ' +date+ '\nX-Coor: "'+ xcoor+'\nY-Coor: ' + ycoor +'\n')
+	# w.writelines([item for item in lastLine] + ",")
+	# w.writelines(',{"layer": "' + layerName +'", "description": "'+ description +'", "date": "' + date + '", "xcoor": "'+ xcoor +'", "ycoor": "' + ycoor + '"}')
+	w.writelines('{\n"date": "' + date + '", \n"description": "' +description + '", \n"layer": "' + layerName+ '", \n"xcoor": "' + xcoor + '", \n"ycoor": "' + ycoor + '"\n}')
+	print ('ADDED: \nLayer Name:' + layerName +'\nDescription : '+ description +'\nDate: ' + date + '\nX-Coor: '+ xcoor +'\nY-Coor: ' + ycoor +'\n')
 	w.writelines("\n]")
 	w.close()
+	clear()
 	
-submitAddButton = Button(root, text ="SUBMIT", command = addLine)
-submitEdit = Button(root, text ="SUBMIT", command = displayLine)
-closeButton = Button(root, text = "CLOSE", command=root.destroy)
-clearButton = Button(root, text='CLEAR ALL', command=clear_textbox) #se if there can be mutliple commands?
+submitAddButton = Button(root, text = "SUBMIT", command = addLine)
+submitEdit = Button(root, text = "SUBMIT", command = modifyLine)
+closeButton = Button(root, text = "CLOSE", command = root.destroy)
 
 def main():
 	add = raw_input("Add Entry [y/n]? ")
-
 	if (add == "Y" or add == "y"):
-
 		addLabel.pack()
 		layerNameText.pack()
 		layerNameEntry.pack()
@@ -125,14 +142,14 @@ def main():
 		xCoorEntry.pack()
 		yCoorText.pack()
 		yCoorEntry.pack()
-		submitAddButton.pack(side =BOTTOM)
-		closeButton.pack(side =BOTTOM)
-		clearButton.pack(side =BOTTOM)
+		submitAddButton.pack(side = BOTTOM)
+		closeButton.pack(side = BOTTOM)
 		root.mainloop() 
 
 	deleteLast  =raw_input("Delete last line [y/n]? ")
 	if (deleteLast == "Y" or deleteLast == "y"):
 		deleteLastLine()
+
 	edit = raw_input("Edit line [y/n]? ")
 	if (edit == "Y" or edit == "y"):
 		changeLayer = raw_input("Layer name to change? ")
@@ -148,12 +165,10 @@ def main():
 		xCoorEntry.pack()
 		yCoorText.pack()
 		yCoorEntry.pack()
-		submitEdit.pack(side =BOTTOM)
-		closeButton.pack(side =BOTTOM)
-		clearButton.pack(side =BOTTOM)
+		submitEdit.pack(side = BOTTOM)
+		closeButton.pack(side = BOTTOM)
 		root.mainloop() 
 		
-
 main()
 
 # {
